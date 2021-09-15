a728bd57af14ac4b3d4d5899f6c79c5df18addce
Loc_IT <loc281202@gmail.com>
2021-09-15 20:20:26 +0700

    template travel

---
+++ main.js
@@ -0,0 +1,70 @@
+// var students = [
+//   {
+//     name: "pham huu loc",
+//     age: 18,
+//     yearBorn: 20,
+//   },
+//   {
+//     name: "pham  thi thu",
+//     age: 18,
+//     yearBorn: 20,
+//   },
+//   {
+//     name: "le thi thuy",
+//     age: 18,
+//     yearBorn: 20,
+//   },
+// ];
+// var totalAge = students.reduce(function (total, currentValue) {
+//   return total + currentValue.age;
+// }, 0);
+// console.log(totalAge);
+
+// create reduce  for js same reduce of js
+Array.prototype.reduce2 = function (callBack, result) {
+  let i = 0; // tao bien chay
+  if (arguments.length < 2) {
+    i = 1;
+    result = this[0];
+  }
+  //sau khi xd đc i
+  for (; i < this.length; i++) {
+    result = callBack(result, this[i], i, this);
+  }
+  return result;
+};
+const number = [1, 2, 3, 4, 5];
+
+const totalNumber = number.reduce2((sum, currentNumber) => {
+  return sum + currentNumber;
+}, 10);
+console.log(totalNumber);
+var numberInteger = [1, 1, 2, 3, 4];
+
+//reduce of js
+var sum = numberInteger.reduce(function (total, number) {
+  console.log(total, number);
+  return total + number;
+});
+//test
+//bt1
+//bt2
+Array.prototype.reduce2 = function (callBack, result) {
+  let i = 0;
+  if (arguments.length < 2) {
+    i = 1;
+    result = this[0];
+  }
+  for (; i < this.length; i++) {
+    result = callBack(result, this[i], i, this);
+  }
+  return result;
+};
+
+var newArray = array.reduce(function (number, currentvalue) {
+  return number.concat(currentvalue);
+}, []);
+console.log(newArray.join(" -- ", "    "));
+console.log("hello");
+var number = 888999;
+console.log("anh loc dep giai qua");
